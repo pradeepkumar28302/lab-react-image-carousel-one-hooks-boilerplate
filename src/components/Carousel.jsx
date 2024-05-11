b9420266f23a1bff1de71f1e2310947948865743
Pradeepkumar.K <98541388+pradeepkumar28302@users.noreply.github.com>
2024-05-11 19:17:59 +0530

    added image carousel using functional component on react js

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,30 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [index,setIndex]=useState(0)
+    const {title,subtitle,img}=images[index];
+
+    const next=()=>{
+        setIndex(index===images.length-1?0:index+1);
+    };
+
+    const prev=()=>{
+        setIndex(index===0?images.length-1:index-1);
+    };
+
+    return (
+        <div className="cards flex">
+            <div onClick={prev} className="left-arrow arrow flex">
+                <ArrowBackIosIcon />
+            </div>
+            <h2 className="title">{title}</h2>
+            <img className="image" src={img} alt="" />
+            <h4 className="caption">{subtitle}</h4>
+            <div onClick={next} className="right-arrow arrow flex">
+                <ArrowForwardIosIcon />
+            </div>
+        </div>
+    );
 }
 
 export default Carousel;
